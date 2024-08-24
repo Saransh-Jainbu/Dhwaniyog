d9e5972d09c60f8420c6fdec3d3b725567e22e26
Ayush Katare <katareayush2005@gmail.com>
2024-08-25 01:34:14 +0530

    landing page completed

--- Login Page/Client/src/App.jsx
+++ Login Page/Client/src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Login from './Login'
+import './App.css'
+import Navbar from './Navbar'
+import Hero from './Hero'
+import CardGrid from './Card'
+import Footer from './Footer'
 
 function App() {
+  return(
+  <>
+      <Navbar />
+      <Hero />
+      <CardGrid />
+      <Footer />
 
-  return (
-   <div>
-    <Login />
-   </div>
-  )
+  </>
+   )
+  
 }
 
 export default App
