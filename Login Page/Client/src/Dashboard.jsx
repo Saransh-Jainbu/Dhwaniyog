2dcca00b7a671fb4103e41aedfeaf444358dcab5
Ayush Katare <katareayush2005@gmail.com>
2024-08-25 17:09:34 +0530

    ui fixes

--- Login Page/Client/src/Dashboard.jsx
+++ Login Page/Client/src/Dashboard.jsx
@@ -7,7 +7,13 @@ const Dashboard = () => {
     <div className="flex">
       
       <div className="w-64 bg-gray-100 h-screen p-4">
-        <h2 className="text-xl font-bold mb-4">Dashboard</h2>
+
+        <Link to="/" className="flex items-center text-gray-700 hover:text-black">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="size-5 mb-4 mr-2">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+        </svg>
+        <h2 className="text-2xl font-bold mb-4">Dashboard</h2>
+        </Link>
         <Link to="/dashboard" className="flex items-center text-gray-700 hover:text-black">
           <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
