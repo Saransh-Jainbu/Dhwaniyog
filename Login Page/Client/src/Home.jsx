b5064cd156a8f5749ba5f453bdcbcff5c009cfe9
Ayush Katare <katareayush2005@gmail.com>
2024-08-25 19:25:46 +0530

    added login page

--- Login Page/Client/src/Home.jsx
+++ Login Page/Client/src/Home.jsx
@@ -3,8 +3,6 @@ import Navbar from './Navbar'
 import Hero from './Hero'
 import CardGrid from './Card'
 import Footer from './Footer'
-import Dashboard from './Dashboard'
-
 
 const Home = () => {
   return (
@@ -13,9 +11,6 @@ const Home = () => {
         <Hero />
         <CardGrid />
         <Footer />
-        {/* <Dashboard /> */}
-        
-
     </>
   )
 }
