e6faafff79609dcc7f41a3b93c15dab6b54d2019
Ayush Katare <katareayush2005@gmail.com>
2024-08-31 17:25:41 +0530

    bug fixes

--- Login Page/Client/src/VerticalLine.jsx
+++ Login Page/Client/src/VerticalLine.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const VerticalLine = () => {
   return (
     <>
-    <div class="flex items-center">
-        <div class="border-r-2 border-gray-300 h-full"></div>
+    <div className="flex items-center">
+        <div className="border-r-2 border-gray-300 h-full"></div>
     </div>
 
     </>
